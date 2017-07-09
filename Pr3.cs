99d9879a8b167714503c2996a62db73dc09b7c84
Dmitry Alexandrov <rus22imp@mail.ru>
2017-07-09 20:35:45 +0500

    Add files via upload

---
+++ Pr3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR3
+{
+    class Pr3
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            Console.Write("Введите количество элементов массива: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] x = new int[n];
+            Console.WriteLine("Введите элементы массива:");
+            for (i = 0; i < n; i++)
+                x[i] = int.Parse(Console.ReadLine());
+            for (i = 0; i < n; i++)
+                if (x[i] < x[i - 1])
+                {
+                    x[i] = x[i + 1];
+                    n--;
+                    i--;
+                }
+            Console.WriteLine("Массив после обработки:");
+            for (i = 0; i < n; i++)
+                Console.WriteLine("{0} ", x[i]);
+            Console.ReadKey();
+        }
+    }
+}
